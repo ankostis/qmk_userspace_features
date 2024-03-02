1dc07c0706d9bfde1ac84fc7aea48c034a996987
Kostis Anagnostopoulos <ankostis@gmail.com>
2024-03-02 15:39:25 +0200

    WIP/feat(maccel) debug-print max velocities >
    WIP because it disabled previous debug-prints.

--- maccel/maccel.c
+++ maccel/maccel.c
@@ -150,9 +150,20 @@ report_mouse_t pointing_device_task_maccel(report_mouse_t mouse_report) {
 
 // console output for debugging (enable/disable in config.h)
 #ifdef MACCEL_DEBUG
-    const float distance_out = sqrtf(x * x + y * y);
-    const float velocity_out = velocity * maccel_factor;
-    printf("MACCEL: DPI:%4i Tko: %.3f Grw: %.3f Ofs: %.3f Lmt: %.3f | Fct: %.3f v.in: %.3f v.out: %.3f d.in: %3i d.out: %3i\n", device_cpi, g_maccel_config.takeoff, g_maccel_config.growth_rate, g_maccel_config.offset, g_maccel_config.limit, maccel_factor, velocity, velocity_out, CONSTRAIN_REPORT(distance), CONSTRAIN_REPORT(distance_out));
+    // const float distance_out = sqrtf(x * x + y * y);
+    // const float velocity_out = velocity * maccel_factor;
+    // printf("MACCEL: DPI:%4i Tko: %.3f Grw: %.3f Ofs: %.3f Lmt: %.3f | Fct: %.3f v.in: %.3f v.out: %.3f d.in: %3i d.out: %3i\n", device_cpi, g_maccel_config.takeoff, g_maccel_config.growth_rate, g_maccel_config.offset, g_maccel_config.limit, maccel_factor, velocity, velocity_out, CONSTRAIN_REPORT(distance), CONSTRAIN_REPORT(distance_out));
+
+    static float max_v_in, max_v_sc, max_a;
+    if (velocity_raw > max_v_in) max_v_in = velocity_raw;
+    if (velocity > max_v_sc) max_v_sc = velocity;
+    if (maccel_factor > max_a) max_a = maccel_factor;
+    if (delta_time > MACCEL_CPI_THROTTLE_MS) {
+        device_cpi = pointing_device_get_cpi();
+        printf("MACCEL: DPI: %4i max.vin: %.3f max.vsc: %.3f max.a: %3f\n", device_cpi, max_v_in, max_v_sc, max_a);
+        max_v_in = max_v_sc = max_a = 0;
+    }
+
 #endif // MACCEL_DEBUG
 
     // report back accelerated values
